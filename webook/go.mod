16beecfa1b2e773d55c77c8ef54a03077496642f
LinSrHang <1023948565@qq.com>
2025-02-01 21:05:22 +0800

    week20 初步接入ELK

--- webook/go.mod
+++ webook/go.mod
@@ -46,6 +46,7 @@ require (
 	golang.org/x/sync v0.10.0
 	google.golang.org/grpc v1.69.4
 	google.golang.org/protobuf v1.36.1
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 	gorm.io/plugin/opentelemetry v0.1.11
--- webook/ioc/logger.go
+++ webook/ioc/logger.go
@@ -4,9 +4,11 @@ import (
 	"github.com/spf13/viper"
 	"github.com/to404hanga/pkg404/logger"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func InitLogger() logger.Logger {
+func InitLogger_OldVersion() logger.Logger {
 	cfg := zap.NewDevelopmentConfig()
 	err := viper.UnmarshalKey("log", &cfg)
 	if err != nil {
@@ -17,4 +19,26 @@ func InitLogger() logger.Logger {
 		panic(err)
 	}
 	return logger.NewZapLogger(l)
+}
+
+func InitLogger() logger.Logger {
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   "E:/VSCode/GO/webook/webook/log/user.log", // 指定日志文件路径
+		MaxSize:    50,                                        // 每个日志文件的最大大小，单位：MB
+		MaxBackups: 3,                                         // 保留旧的日志文件最大个数
+		MaxAge:     28,                                        // 保留旧的日志文件最大天数
+		Compress:   true,                                      // 是否压缩旧的日志文件
+	}
+
+	// 创建 zap 日志核心
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(lumberjackLogger),
+		zapcore.DebugLevel, // 设置日志级别
+	)
+
+	l := zap.New(core, zap.AddCaller())
+	res := logger.NewZapLogger(l)
+
+	return res
 }
